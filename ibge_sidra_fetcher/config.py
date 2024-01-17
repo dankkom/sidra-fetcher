c3c9f95e7db6198d778b15b0f5c0c80e6f27e9a1
Daniel Komesu <danielkomesu@gmail.com>
2024-01-16 21:32:13 -0300

    Change CONFIG_DIR

--- ibge_sidra_fetcher/config.py
+++ ibge_sidra_fetcher/config.py
@@ -3,7 +3,7 @@ import logging.config
 import os
 from pathlib import Path
 
-CONFIG_DIR = os.getenv("CONFIG_DIR")
+CONFIG_DIR = Path()
 if not CONFIG_DIR:
     raise FileNotFoundError("No configuration directory found in environment variables")
 CONFIG_DIR = Path(CONFIG_DIR)
@@ -14,7 +14,7 @@ DATA_DIR = Path(DATA_DIR) / "raw" / "ibge" / "sidra"
 DATA_DIR.mkdir(exist_ok=True, parents=True)
 
 _config = configparser.ConfigParser()
-_config.read(CONFIG_DIR / "ibge-sidra" / "config.ini")
+_config.read(CONFIG_DIR / "config.ini")
 
 USER_AGENT = _config["DEFAULT"]["USER_AGENT"]
 HTTP_HEADERS = {
@@ -22,7 +22,7 @@ HTTP_HEADERS = {
 }
 TIMEOUT = int(_config["DEFAULT"]["TIMEOUT"])
 
-_logging_config_filepath = CONFIG_DIR / "ibge-sidra" / "logging.ini"
+_logging_config_filepath = CONFIG_DIR / "logging.ini"
 if not _logging_config_filepath.exists():
     raise FileNotFoundError("No logging configuration file exists")
 logging.config.fileConfig(_logging_config_filepath)
