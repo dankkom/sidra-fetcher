3f31e7be4f44ab0b58e0038f21645d6a07fac652
Daniel Komesu <danielkomesu@gmail.com>
2024-09-27 21:50:57 -0300

    Add script 02.3-get-agregados-periodos.py

---
+++ 02.3-get-agregados-periodos.py
@@ -0,0 +1,44 @@
+import argparse
+import queue
+from pathlib import Path
+
+from ibge_sidra_fetcher import dispatcher, fetcher, storage
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data-dir",
+        type=Path,
+        required=True,
+        help="Directory to store the fetched data",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    data_dir = args.data_dir
+
+    sidra_agregados_filepath = storage.sidra_agregados_filepath(data_dir=data_dir)
+    sidra_agregados_data = storage.read_json(sidra_agregados_filepath)
+
+    q = queue.Queue()
+    for _ in range(4):
+        fetcher_worker = fetcher.Fetcher(q)
+        fetcher_worker.start()
+
+    for pesquisa in sidra_agregados_data:
+        pesquisa_id = pesquisa["id"]
+        for agregado in pesquisa["agregados"]:
+            agregado_id = int(agregado["id"])
+            task = dispatcher.periodos(data_dir, pesquisa_id, agregado_id)
+            if task["dest_filepath"].exists():
+                continue
+            print("Task periodos:", pesquisa_id, agregado_id)
+            q.put(task)
+    q.join()
+
+
+if __name__ == "__main__":
+    main()
