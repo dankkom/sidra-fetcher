39fb1c862abe5d33a3afbd6eaea81a3a39cfa2dd
Komesu, D.K. <daniel@dkko.me>
2026-01-01 20:19:00 -0300

    chore: update copyright year in multiple source files

--- src/sidra_fetcher/__init__.py
+++ src/sidra_fetcher/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2026 Daniel Kiyoyudi Komesu
+# Copyright (C) 2022-2026 Daniel Kiyoyudi Komesu
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
